9246f26d5b32f9da495448bb05aa710e8de8ec2d
Thomas Twardoch <thomas.twardoch@gmx.de>
2024-09-12 21:59:56 +0200

    Update RAG

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from langchain import langsmith
 from langchain.document_loaders import PyPDFLoader
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
@@ -7,17 +6,37 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
+embedding_model = "text-embedding-3-large"
+
 file_path = (
-    "Testdaten\10400552_Festgeld_3_Monate.pdf"
+    "Testdaten/10400552_Festgeld_3_Monate.pdf"
 )
 
 loader = PyPDFLoader(file_path)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=50)
+docs = loader.load()
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300, chunk_overlap=50)
 
-vector_store = Chroma(
+chunks = text_splitter.split_documents(docs)
+
+print(f"Split {len(docs)} documents into {len(chunks)} chunks.")
+
+embeddings = OpenAIEmbeddings(
+    model=embedding_model)
+
+vector_store = Chroma.from_documents(
+    documents=chunks,
     collection_name="pdf_collection",
-    embedding_function=embeddings
+    embedding=embeddings
 )
+
+retriever = vector_store.as_retriever()
+
+retrieved_documents = retriever.invoke("Wie hoch ist die Mindesteinlage?")
+
+# Zeige gefundene Datensätze aus Vektordatenbank
+print(retrieved_documents[0].page_content)
+print(retrieved_documents[0].metadata)
+print(len(retrieved_documents))
