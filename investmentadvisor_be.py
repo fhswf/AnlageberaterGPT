2fb4cbed8c94a8af46b016514c2a6bc147bf0836
Thomas Twardoch <thomas.twardoch@gmx.de>
2024-10-07 00:35:06 +0200

    Ausimplementierung Produktvorschlag + Update Embedding

--- investmentadvisor_be.py
+++ investmentadvisor_be.py
@@ -1,9 +1,13 @@
+import io
 import json
 from typing import TypedDict, Annotated, Sequence
 
-from IPython.core.display import Image
+from PIL import Image
 from dotenv import load_dotenv
+from langchain_chroma import Chroma
 from langchain_core.messages import ToolMessage, BaseMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
@@ -31,12 +35,74 @@ class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
 
+# TypedDict
+class InvestmentMetadata(TypedDict):
+    """extrahierten Metadaten um ein passendes Produkt zu suchen"""
+
+    mindestanlagebetrag: Annotated[int, ..., "Der Betrag den der Kunde anlegen möchte"]
+    anlagedauer: Annotated[
+        str, ..., "Die Dauer, wie lange der Kunde das Geld anlegen möchte (kurzfristig, mittelfristig, langfristig)"]
+    risikobereitschaft: Annotated[
+        str, ..., "Wie Risikobereit ist der Kunde (kein Risiko, mittleres Risiko, hohes Risiko"]
+
+
 # Definiere Tool um Metadaten aus den Antworten des Anwenders zu extrahieren
 @tool
 def retrieve_metadata(prompt: str):
-    """Extrahiert Metadaten aus den Antworten"""
-    price = 5000
-    return f"Das aktuelle Produkt hat den Preis ${price:.2f}"
+    """Bestimmt beziehungsweise extrahiert mithilfe GPT-Modell Metadaten aus den Antworten"""
+
+    prompt = """Für die folgenden Fragen extrahierst du aus den Antworten Informationen zum Kunden, die später 
+    benötigt werden: - Wieviel möchten Sie anlegen? Hier soll aus der Antwort nur der Betrag entnommen werden, 
+    den der Kunde anlegen möchte. - Für wie lange können Sie das Geld anlegen (kurzfristig, mittelfristig, 
+    langfristig)? Hier sollst du aus der Antwort des Kunden bestimmen, ob der Kunde kurzfristig, mittelfristig oder 
+    langfristig anlegt. Antworte nur mit einem der Möglichkeiten. - Wie hoch ist Ihre Bereitschaft, Verluste in Kauf 
+    zu nehmen? Hier sollst du die Risikobereitschaft aus der Antwort des Kunden auswerten. Hierfür sollst du eines 
+    der Möglichkeiten ausgeben: kein Risiko, mittleres Risiko, hohes Risiko
+    Der Kunde hat folgende Antworten (nacheinander) gegeben: """ + prompt
+
+    structured_llm = llm.with_structured_output(InvestmentMetadata)
+    answer = structured_llm.invoke(prompt)
+    return {"messages": answer}
+
+
+# ToDo: Umstrukturieren, sodass Langgraph den Workflow bestimmt und nicht GPT
+# Definiere Tool um Metadaten aus den Antworten des Anwenders zu extrahieren
+def get_productdata(state: AgentState):
+    """Filtert mithilfe der extrahierten Metadaten die passenden Produktinformationen"""
+    context = state["messages"][-1]
+    metadata_json = json.loads(context.content)
+    metadata_value = metadata_json["messages"]
+    print(metadata_value)
+    mindestanlagebetrag = metadata_value["mindestanlagebetrag"]
+    # anlagedauer = metadata_value["anlagedauer"]
+    risikobereitschaft = metadata_value["risikobereitschaft"]
+
+    vectordb_full_documents = Chroma(persist_directory="./chroma_langchain_db",
+                                     collection_name="pdf_collection_documents",
+                                     embedding_function=embeddings)
+
+    # Filterung der Dokumente nach Metadaten
+    retriever = vectordb_full_documents.as_retriever(
+        search_kwargs={"filter": {
+            '$and': [{'mindestanlagebetrag': {'$eq': mindestanlagebetrag}},
+                     {'risiko': {'$eq': risikobereitschaft}}]},
+        })
+
+    # Abfrage Daten aus Vektordatenbank
+    retrieved_documents = retriever.invoke("")
+    print(retrieved_documents)
+
+    prompt = PromptTemplate(
+        template="""Du bist ein Anlageberater von der Musterbank eG und hast soeben ein passendes Produkt gefunden, 
+        dass dem Kunden angeboten werden kann. Nutze die folgenden abgerufene Dokumente, um kurz das Produkt 
+        vorzustellen und die relevanten Informationen zusammenzufassen. Es handelt sich hierbei um ein 
+        Produktinformationsblatt.
+        Hier sind die Informationen zum Produkt: {context}""", input_variables=[context])
+
+    llm_produktvorstellung = prompt | llm | StrOutputParser()
+    answer = llm_produktvorstellung.invoke({"context": retrieved_documents})
+
+    return {"messages": [answer]}
 
 
 # Liste mit allen Tools die verwendet werden sollen, um passendes Produkt zu finden
@@ -75,7 +141,8 @@ def call_model(
     - Wie hoch ist Ihre Bereitschaft, Verluste in Kauf zu nehmen?
     - Bevorzugen Sie bestimmte Anlageformen (z. B. Immobilien, Aktien, Anleihen, Kryptowährungen)?
 
-    Rufe einen Tool auf, um die Metadaten aus den Antworten zu extrahieren."""
+    Rufe einen Tool auf, um die Metadaten aus den Antworten zu extrahieren. Anschließend wird eine Funktion 
+    aufgerufen, um ein passendes Produkt zu finden."""
 
     response = tool_llm.invoke([system_prompt] + state["messages"])
     # We return a list, because this will get added to the existing list
@@ -100,6 +167,7 @@ workflow = StateGraph(AgentState)
 # Define the two nodes we will cycle between
 workflow.add_node("agent", call_model)
 workflow.add_node("tools", tool_node)
+workflow.add_node("product", get_productdata)
 
 # Set the entrypoint as `agent`
 # This means that this node is the first one called
@@ -126,19 +194,17 @@ workflow.add_conditional_edges(
     },
 )
 
+workflow.add_edge("tools", "product")
 # We now add a normal edge from `tools` to `agent`.
 # This means that after `tools` is called, `agent` node is called next.
-workflow.add_edge("tools", "agent")
+workflow.add_edge("product", "agent")
 
 # Now we can compile and visualize our graph
 graph = workflow.compile()
 
-
-# try:
-#     img = Image(graph.get_graph().draw_mermaid_png())
-# except Exception:
-#     # This requires some extra dependencies and is optional
-#     pass
+# testimg = graph.get_graph().draw_mermaid_png()
+# img = Image.open(io.BytesIO(testimg))
+# img.show()
 
 def print_stream(stream):
     for s in stream:
@@ -149,22 +215,9 @@ def print_stream(stream):
             message.pretty_print()
 
 
-inputs = {"messages": [("user", "5000 €, kurzfristig, Ich habe eine hohe Bereitschaft, Nein")]}
+inputs = {"messages": [("user", "0 €, kurzfristig, Ich gehe kein Risiko ein, Nein")]}
 
 print_stream(graph.stream(inputs, stream_mode="values"))
 
-# ToDo: Ueber Langchain kann man die Fragen nicht nacheinander stellen (Sollte ueber UI geschehen)
 # ToDo: Bestimme Metadaten aus Antworten
 # ToDo: Dynamische Suche mit Metadaten
-
-# vectordb = Chroma(persist_directory="./chroma_langchain_db", collection_name="pdf_collection",
-#                   embedding_function=embeddings)
-
-# Filterung der Dokumente nach Metadaten
-# retriever = vectordb.as_retriever(
-#     search_kwargs={"filter": {
-#         '$and': [{'produktnummer': {'$eq': 10400552}}, {'risiko': {'$eq': 'Kein Risiko'}}]},
-#         "k": 1})
-
-# Abfrage Daten aus Vektordatenbank
-# retrieved_documents = retriever.invoke("Wie hoch ist die Mindesteinlage?")
--- product_embedding.py
+++ product_embedding.py
@@ -15,61 +15,72 @@ embedding_model = "text-embedding-3-large"
 folder_path = "testdaten"
 csv_path = "produkteinstufung/ProduktMetadaten.csv"
 
+splitting_option = ["pdf_collection_chunks", "pdf_collection_documents"]
 
-# CSV-Datei einlesen (mit pandas) fuer Metadaten
-def load_metadata_from_csv(path):
-    return pd.read_csv(path, sep=';')
-
-
-# Mapper Metadaten pro Dokument
-def map_metadata(doc, metadaten):
-    doc.metadata["produktname"] = metadaten["Produktname"].item()
-    doc.metadata["produktnummer"] = metadaten["Produktnummer"].item()
-    doc.metadata["mindestanlagebetrag"] = metadaten["Mindestanlagebetrag"].item()
-    doc.metadata["laufzeit"] = metadaten["Laufzeit"].item()
-    doc.metadata["risiko"] = metadaten["Risiko"].item()
-    doc.metadata["gutschrift"] = metadaten["Gutschrift"].item()
-    doc.metadata["kosten"] = metadaten["Kosten"].item()
+for option in splitting_option:
+    # CSV-Datei einlesen (mit pandas) fuer Metadaten
+    def load_metadata_from_csv(path):
+        return pd.read_csv(path, sep=';')
 
 
-def load_pdfs_from_folder(path):
-    # Liste alle Dateien im Ordner auf
-    pdf_files = [f for f in os.listdir(path) if f.endswith('.pdf')]
-
-    # CSV-Datei laden
-    metadata_df = load_metadata_from_csv(csv_path)
-
-    # Initialisiere Liste fuer Dokumente
-    document_list = []
-
-    # Lese alle PDFs im Ordner ein
-    for pdf_file in pdf_files:
-        produkt_metadaten = metadata_df[metadata_df['Dateiname'] == pdf_file]
-        full_path = os.path.join(path, pdf_file)
-        loader = PyPDFLoader(full_path)
-        document = loader.load()
-        for doc in document:
-            map_metadata(doc, produkt_metadaten)
-        document_list.extend(document)  # Füge die Dokumente hinzu
-
-    return document_list
+    # Mapper Metadaten pro Dokument
+    def map_metadata(doc, metadaten):
+        doc.metadata["produktname"] = metadaten["Produktname"].item()
+        doc.metadata["produktnummer"] = metadaten["Produktnummer"].item()
+        doc.metadata["mindestanlagebetrag"] = int(metadaten["Mindestanlagebetrag"].item())
+        doc.metadata["laufzeit"] = metadaten["Laufzeit"].item()
+        doc.metadata["risiko"] = metadaten["Risiko"].item()
+        doc.metadata["gutschrift"] = metadaten["Gutschrift"].item()
+        doc.metadata["kosten"] = metadaten["Kosten"].item()
 
 
-documents = load_pdfs_from_folder(folder_path)
+    def load_pdfs_from_folder(path):
+        # Liste alle Dateien im Ordner auf
+        pdf_files = [f for f in os.listdir(path) if f.endswith('.pdf')]
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100, chunk_overlap=50)
+        # CSV-Datei laden
+        metadata_df = load_metadata_from_csv(csv_path)
 
-chunks = text_splitter.split_documents(documents)
+        # Initialisiere Liste fuer Dokumente
+        document_list = []
 
-print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+        # Lese alle PDFs im Ordner ein
+        for pdf_file in pdf_files:
+            produkt_metadaten = metadata_df[metadata_df['Dateiname'] == pdf_file]
+            full_path = os.path.join(path, pdf_file)
+            loader = PyPDFLoader(full_path)
+            document = loader.load()
+            for doc in document:
+                map_metadata(doc, produkt_metadaten)
+            document_list.extend(document)  # Füge die Dokumente hinzu
 
-embeddings = OpenAIEmbeddings(
-    model=embedding_model)
+        return document_list
 
-vector_store = Chroma.from_documents(
-    documents=chunks,
-    collection_name="pdf_collection",
-    embedding=embeddings,
-    persist_directory="./chroma_langchain_db"
-)
+
+    documents = load_pdfs_from_folder(folder_path)
+
+    embeddings = OpenAIEmbeddings(
+        model=embedding_model)
+
+    if option == "pdf_collection_chunks":
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=100, chunk_overlap=50)
+
+        chunks = text_splitter.split_documents(documents)
+
+        print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+        vector_store = Chroma.from_documents(
+            documents=chunks,
+            collection_name=option,
+            embedding=embeddings,
+            persist_directory="./chroma_langchain_db"
+        )
+    else:
+        print("No Splitting")
+        vector_store = Chroma.from_documents(
+            documents=documents,
+            collection_name=option,
+            embedding=embeddings,
+            persist_directory="./chroma_langchain_db"
+        )
